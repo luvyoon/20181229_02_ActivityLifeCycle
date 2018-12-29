cc279e5bfcae15c33f63dce8cfbc40ffbce685b7
Hottory <gkffndnl123@naver.com>
2018-12-29 11:49:52 +0900

    두 액티비티 이동

--- app/src/main/java/kr/tjeit/a20181229_02_activitylifecycle/NextActivity.java
+++ app/src/main/java/kr/tjeit/a20181229_02_activitylifecycle/NextActivity.java
@@ -1,38 +1,36 @@
 package kr.tjeit.a20181229_02_activitylifecycle;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends BaseActivity {
+public class NextActivity extends BaseActivity {
 
-
-    private android.widget.Button moveBtn;
+    private android.widget.Button closeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_next);
 
         bindviews();
         setupEvents();
         setvalues();
+
     }
 
     @Override
     public void setupEvents() {
 
-        moveBtn.setOnClickListener(new View.OnClickListener() {
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(mContext,NextActivity.class);
-                startActivity(intent);
-
+                finish();
 
             }
         });
+
 
     }
 
@@ -43,8 +41,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindviews() {
-
-        this.moveBtn = (Button) findViewById(R.id.moveBtn);
+        this.closeBtn = (Button) findViewById(R.id.closeBtn);
 
     }
 }
